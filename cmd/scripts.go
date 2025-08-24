ac517e6e2f18b1ec3945b6549e1ac2c08bdef432
JohanGJensen <johancentralen@gmail.com>
2025-08-24 20:21:01 +0200

    refactor scripts to allow recursive lookup

--- cmd/scripts.go
+++ cmd/scripts.go
@@ -18,8 +18,8 @@ type CommandObject struct {
 	Value string
 }
 
-func runSelectPrompt(pkgJson models.PackageJson) string {
-	scripts := pkgJson.Scripts
+func runSelectPrompt(pkgSlice models.PackageJsonSlice) string {
+	scripts := pkgSlice.GetAllScripts()
 	keys := make([]string, 0, len(scripts))
 	values := make([]string, 0, len(scripts))
 
@@ -65,19 +65,45 @@ func runConfirmSelectPrompt(cmd string) string {
 	return cmd
 }
 
-func getCmdFromPackageJSON() (result string) {
-	file, err := os.ReadFile("package.json")
-	if err != nil {
-		log.Fatalf("No file named 'package.json' in this directory: '%s'\n", err)
+func getCmdFromPackageJSON(path string, recursive bool) (result string) {
+	var pkgSlice models.PackageJsonSlice
+
+	if recursive {
+		readDirectoryContentRecursive(path, &pkgSlice)
+	} else {
+		readDirectoryContent(path, &pkgSlice)
 	}
 
-	var pkgJson models.PackageJson
-	pkgJson.SetScripts(file)
-
-	selectedCmd := runSelectPrompt(pkgJson)
+	log.Println(pkgSlice)
+	selectedCmd := runSelectPrompt(pkgSlice)
 	confirmedCmd := runConfirmSelectPrompt(selectedCmd)
 
 	return confirmedCmd
+}
+
+func readDirectoryContent(path string, pkgSlice *models.PackageJsonSlice) {
+	var pkgJson models.PackageJson
+
+	file, err := os.ReadFile(path + "package.json")
+	if err == nil {
+		pkgJson.SetScripts(file)
+		pkgSlice.AppendPackageJson(pkgJson)
+	}
+}
+
+func readDirectoryContentRecursive(path string, pkgSlice *models.PackageJsonSlice) {
+	readDirectoryContent(path, pkgSlice)
+
+	entries, err := os.ReadDir(path)
+	if err != nil {
+		log.Fatalln("failed to read directory", err)
+	}
+
+	for _, entry := range entries {
+		if entry.IsDir() && !strings.HasPrefix(entry.Name(), ".") {
+			readDirectoryContentRecursive(path+entry.Name()+"/", pkgSlice)
+		}
+	}
 }
 
 var scriptsCmd = &cobra.Command{
@@ -85,15 +111,20 @@ var scriptsCmd = &cobra.Command{
 	Short: "Lists all script commands from package.json file",
 	Long:  "Lists all script commands from package.json file",
 	Run: func(cmd *cobra.Command, args []string) {
-		command := getCmdFromPackageJSON()
+		recursive, err := cmd.Flags().GetBool("recursive")
+		if err != nil {
+			log.Fatalln("could not read recursive flag properly")
+		}
+
+		command := getCmdFromPackageJSON("./", recursive)
 		/**
 		 * Passing a command as a single string like this is not a safe pattern.
 		 * Risk of shell injection.
 		 * Reconsider this approach.
 		 */
-		err := exec.Command("bash", "-c", command).Run()
-		if err != nil {
-			log.Fatalln("could not run command: ", err)
+		runErr := exec.Command("bash", "-c", command).Run()
+		if runErr != nil {
+			log.Fatalln("could not run command: ", runErr)
 		}
 
 		os.Exit(1)
--- models/package.go
+++ models/package.go
@@ -6,12 +6,31 @@ import (
 )
 
 type PackageJson struct {
+	Name    string            `json:"name"`
 	Scripts map[string]string `json:"scripts"`
 }
 
 func (p *PackageJson) SetScripts(file []byte) {
 	err := json.Unmarshal(file, &p)
 	if err != nil {
-		log.Fatal("Failed to marshal JSON from file.")
+		log.Fatal("Failed to unmarshal JSON from file.")
 	}
+}
+
+type PackageJsonSlice []PackageJson
+
+func (p *PackageJsonSlice) AppendPackageJson(pkg PackageJson) {
+	*p = append(*p, pkg)
+}
+
+func (p *PackageJsonSlice) GetAllScripts() map[string]string {
+	scripts := make(map[string]string)
+
+	for _, pkg := range *p {
+		for key, script := range pkg.Scripts {
+			scripts[pkg.Name+" - "+key] = script
+		}
+	}
+
+	return scripts
 }
